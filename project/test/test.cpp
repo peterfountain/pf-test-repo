7b1157a03e01cd598983954742f86941727348b0
Peter Fountain <fountainpeter@googlemail.com>
2015-07-29 22:04:22 +0100

    test.cpp: Added some c++11.

--- project/test/test.cpp
+++ project/test/test.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include "hello.h"
+#include <vector>
 
 using namespace std;
 
 int main(int argv, char *argc[])
 {
-	hello("Pete");	
+	hello("Pete");
 	hello("Ted");
+
+	vector<string> strings;
+	strings.push_back("Hello");
+	strings.push_back("Goodbye");
+
+	for (string s : strings)
+		cout << s << endl;
 
 	return 0;
 }
